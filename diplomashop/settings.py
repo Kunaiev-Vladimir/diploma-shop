966d9953586a737af12f4dd28472e27ec38e9b44
Kunaiev-Vladimir <140187@ukr.net>
2026-05-04 11:17:25 +0300

    Обновил 04_05_2026 connect postgres database

--- diplomashop/settings.py
+++ diplomashop/settings.py
@@ -106,9 +106,23 @@ WSGI_APPLICATION = 'diplomashop.wsgi.application'
     #}
 #}
 
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-}
+#DATABASES = {
+    #'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+#}
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
